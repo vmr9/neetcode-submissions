bcf7da1c7972041749a4b280d80fe90a6cfbcbc1
vmr9 <43559618+vmr9@users.noreply.github.com>
2026-07-22 06:50:59 -0400

    Add: remove-element - submission-2

---
+++ Data Structures & Algorithms/remove-element/submission-2.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i = 0, n = nums.length;
+        while (i < n) {
+            if (nums[i] == val) {
+                nums[i] = nums[--n];
+            } else {
+                i++;
+            }
+        }
+        return n;
+    }
+}
