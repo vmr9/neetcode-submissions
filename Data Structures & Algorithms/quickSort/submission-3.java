a8d3474c834804d505443bd03f1ada0ac2858b22
vmr9 <43559618+vmr9@users.noreply.github.com>
2026-08-25 06:28:13 -0400

    Add: quickSort - submission-3

---
+++ Data Structures & Algorithms/quickSort/submission-3.java
@@ -0,0 +1,42 @@
+// Definition for a pair.
+// class Pair {
+//     int key;
+//     String value;
+//
+//     public Pair(int key, String value) {
+//         this.key = key;
+//         this.value = value;
+//     }
+// }
+class Solution {
+    public List<Pair> quickSort(List<Pair> pairs) {
+
+        quickSortHelper(pairs, 0, pairs.size()-1);
+        return pairs;
+    }
+
+    public void quickSortHelper(List<Pair> pairs, int s, int e) {
+
+        if(e-s+1 <= 1) {
+            return;
+        }
+
+        Pair pivot = pairs.get(e);
+        int left = s;
+
+        for(int i=s; i<e; i++) {
+            if(pairs.get(i).key < pivot.key){
+                Pair temp = pairs.get(left);
+                pairs.set(left, pairs.get(i));
+                pairs.set(i, temp);
+                left++;
+            }
+        }
+
+        pairs.set(e, pairs.get(left));
+        pairs.set(left, pivot);
+
+        quickSortHelper(pairs, s, left-1);
+        quickSortHelper(pairs, left+1, e);
+    }
+}
