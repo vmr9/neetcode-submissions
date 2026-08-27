449be43e9e90cfe8dd71dc598d03a72340646a9c
vmr9 <43559618+vmr9@users.noreply.github.com>
2026-08-27 06:46:00 -0400

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+
+        int l = 0;
+        int r = nums.length-1;
+
+        while(l <= r) {
+            int m = (l+r)/2;
+            if(nums[m] > target) {
+                r = m-1;
+            } else if(nums[m] < target) {
+                l = m+1;
+            } else {
+                return m;
+            }
+        }
+        return -1;
+    }
+}
